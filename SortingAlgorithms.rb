4708600fbfdf51ec8312d59eadcf877c3f27e5fb
Jamie <jamiewong63@googlemail.com>
2020-08-29 00:23:52 +0100

    fixed: selection sort bug

--- SortingAlgorithms.rb
+++ SortingAlgorithms.rb
@@ -1,5 +1,9 @@
 class SortingAlgorithms
 
+  def displayArray(arr)
+    p arr
+  end
+
   def createArray(size)
     Array.new(size) { rand(0..100) }
   end
@@ -7,15 +11,20 @@ class SortingAlgorithms
   def selectionSort(arr)
     # Go through the array and pick the minimum element and place at the front
     # Then search through the array again from the next element onwards first loop start 0, second start at 1 etc
-    ans = [];
+    ans = arr
     for i in 0...arr.length do
+      minIndex = i
       min = arr[i]
       for j in i...arr.length do
-        min = arr[j] if arr[i] > arr[j]
+        if arr[minIndex] > arr[j]
+          minIndex = j
+          min = arr[j]
+        end
       end
-      ans.push(min)
+      arr[minIndex] = arr[i]
+      arr[i] = min
     end
-    ans
+    arr
   end
 
 end
