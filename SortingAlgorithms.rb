c0f085c84e873e23374928e5e4c1544cc9955c1b
Jamie <jamiewong63@googlemail.com>
2020-08-30 16:42:04 +0100

    insertion sort implementation

--- SortingAlgorithms.rb
+++ SortingAlgorithms.rb
@@ -27,4 +27,21 @@ class SortingAlgorithms
     arr
   end
 
+  def insertionSort(arr)
+    # 1: Iterate from arr[1] to arr[n] over the array.
+    # 2: Compare the current element (key) to its predecessor.
+    # 3: If the key element is smaller than its predecessor, compare it to the elements before. Move the greater elements one position up to make space for the swapped element.
+    for i in 1...arr.length do
+      key = arr[i]
+      j = i - 1
+      # Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+      while (j >= 0 && arr[j] > key) do
+        arr[j + 1] = arr[j]
+        j = j - 1
+      end
+      arr[j + 1] = key
+    end
+    arr
+  end
+
 end
